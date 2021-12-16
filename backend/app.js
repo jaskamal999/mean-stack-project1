e7733e6b4a5c1d36d6029dba0dd9eeffa62852e8
Jaskamal Singh <jaskamal999@gmail.com>
2021-12-15 17:42:37 -0800

    fetching initial posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("first middleware");
-    next();
-});
+app.use('/posts' ,(req, res, next) => {
+    const posts = [
+        {id: "jsadksad", title: "first title", content: "first content"},
+        {id: "djsakljasld", title: "second title", content: "second content"}
+    ];
+    // could have also just done res.json(posts) but demonstrating that complex objects can also be sent. 
+    // Also add chain method .status(200) to check success status before returning the response
+    // No need to add return before res as it is the last statement it will be executed 
+    res.status(200).json({
+        message: "post fetched successfully",
+        posts: posts
+    });
 
-app.use((req, res, next) => {
-    res.send("hello from express");
+    // Note: No next() call here as we dont want to do anything after this 
 });
 
 module.exports = app;
